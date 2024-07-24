898a3c7d072b51c29d70b9ae68637a24435389a0
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-07-24 12:41:32 +0100

    Introduced request module in scraper.get_html for obtaining request object from url.

--- scraper.py
+++ scraper.py
@@ -2,10 +2,20 @@
 # I: A verb or container of verbs.
 # O: The verb conjugations.
 
+from bs4 import BeautifulSoup
+import requests
+
 class scraper:
 
     def __init__(self, url) -> None:
         self.url = url
 
     def get_html(self):
-        return self.url is not None
+        try:
+            response = requests.get(self.url)
+        except requests.exceptions.RequestException:
+            print(f"Error obtaining response from {self.url}.")
+            return None
+        return response is not None
+    
+
