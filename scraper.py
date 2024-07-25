7fa03cea8dfde7a732ff75d9d6ec60f2f4e4832d
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-07-25 10:32:19 +0100

    Added functionality to scraper.extract_text_from_html_tags (renamed), extracts the text from the tags and creates a dictioanry of dictionaries, which contains the tense, conjugations, and whether the conjugation is irregular.

--- scraper.py
+++ scraper.py
@@ -17,21 +17,55 @@ class scraper:
             }
 
     def get_html(self):
+        """
+        """
         try:
             response = requests.get(self.url, headers=self.headers)
         except requests.exceptions.RequestException:
-            print(f"Error obtaining response from {self.url}.")
             return None
-        soup = BeautifulSoup(response.content, "html.parser")
-        return soup
+        return BeautifulSoup(response.content, "html.parser")
     
-    def parser(self):
-        pass
+    def extract_text_from_html_tags(self):
+        """
+        """
+        bs = self.get_html()
+        if not bs:
+            return f"Error obtaining response from {self.url}."
 
-    def get_conjugations(self):
+        # verbtxt_tags = bs.find_all('i', class_='verbtxt')
+        # verbtxt_term_irr_tags = bs.find_all('i', class_='verbtxt-term')
+        # verbtxt_term_irr_tags + bs.find_all('i', class_='verbtxt-term-irr')
+        # ----- Reverso HTML Exactraction -----
+        text = {}
+        tense = None
+        word_constructor = None
+        for element in bs.descendants:
+            if element.name:   # All HTML tags have name attributes.
+                if element.get('mobile-title', []):
+                    tense = element['mobile-title']
+                    text[tense] = {}
+                if 'verbtxt' in element.get('class', []):
+                    word_constructor = element.get_text()
+                if 'verbtxt-term' in element.get('class', []):
+                    word_constructor += element.get_text()
+                    text[tense][word_constructor] = False
+                    word_constructor = None
+                if 'verbtxt-term-irr' in element.get('class', []):
+                    word_constructor += element.get_text()
+                    text[tense][word_constructor] = True
+                    word_constructor = None
+                
+        for tense, words in text.items():
+            print(f"Tense: {tense}")
+            for word, value in words.items():
+                print(f"  Word: {word} : {value}")
+            print()  # Print a newline for better readability
+
+
+    def store_conjugations(self):
         pass            
     
 
-x = scraper("https://conjugator.reverso.net/conjugation-portuguese.html")
-print(x.get_html())
+x = scraper("https://conjugator.reverso.net/conjugation-portuguese-verb-poder.html")
+print(x.extract_text_from_html_tags())
 
