6b94e27a448370f8b1beda38f38a30d807f9e72d
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-07-29 13:41:32 +0100

    Altered error catching when connecting to server.

--- MySQL_connection.py
+++ MySQL_connection.py
@@ -1,4 +1,4 @@
-""" Connect to MySQL database using user: python_pt """
+""" Connect to MySQL server or database (if given) using user: python_pt password: 123. """
 
 import mysql.connector
 from mysql.connector import errorcode
@@ -6,26 +6,23 @@ from mysql.connector import errorcode
 connection_args = {
   "host":"localhost", 
   "user":"python_pt", 
-  "password":"1234"
-  # database = "database_name"   # Connect to databse at login.
+  "password":"123",
+  "database": None   # Connect to databse at login.
   }
   
 def connect_to_mysql_server(args=connection_args):
   try:
     connection = mysql.connector.connect(**args)
     if connection.is_connected():
-      print("Connected to server!")
       return connection
   except mysql.connector.Error as error:
-    if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:   
-      print(("Something is wrong with your user name or password"))
-      raise ValueError from error
-    elif error.errno == errorcode.ER_BAD_DB_ERROR:        
-      print("Database does not exist")
-      raise ValueError from error
+    print(f"ERROR CONNECTING TO SERVER\nDEBUG INFO\nError number: {error.errno}\nError message: {error.msg}")
+    return None
+
   else:
     print("Closing connection from MySQL_connection.connect_to_mysql_server()")
     connection.close()
+    return "Error occured"
 
 # print(mydb) # <mysql.connector.connection_cext.CMySQLConnection object at 0x0000012BA05D1D60> PS C:\Users\lconn\Documents\Programming\Python\Python Projects\Portuguese_Verb_Database> 
 
