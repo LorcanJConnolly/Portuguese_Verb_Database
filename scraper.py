1eae61863c062e2a6ea40690803a665a0638484c
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-07-30 13:03:28 +0100

    Added funcitonality for adding to and returning the global_dataset of scraped verb conjugations. Added functionality for adding the root_verb layer of the global_dataset.

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ import requests
 
 class Scraper:
 
-    def __init__(self, url) -> None:
+    def __init__(self, url, verb, global_dataset) -> None:
         self.url = url
         self.headers = headers = {
             'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
@@ -15,7 +15,8 @@ class Scraper:
             'Accept':'text/html,application/xhtml+xml,application/xml;'
             'q=0.9,image/webp,*/*;q=0.8'
             }
-        self.verb = None
+        self.verb_root = verb
+        self.global_dataset = global_dataset
 
     def get_html(self):
         """
@@ -35,8 +36,10 @@ class Scraper:
         
         # ----- Reverso HTML Exactraction -----
         # Add the root verb layer to the dictionary and its key.
-        text = {}
-        whie_text = {}
+        # Whatever the dataset was before, we add the self.verb to it.
+        verb_dataset = {}
+        if self.verb_root not in self.global_dataset:
+            self.global_dataset[self.verb_root] = verb_dataset
         tense = None
         pronoun = None
         word_constructor = None
@@ -45,7 +48,7 @@ class Scraper:
             if element.name:   # All HTML tags have name attributes.
                 if "blue-box-wrap" in element.get('class', []) and element.get('mobile-tile', []) is not None:
                     tense = element["mobile-title"]
-                    text[tense] = []
+                    verb_dataset[tense] = []
                 if 'graytxt' in element.get('class', []):
                     pronoun = element.get_text()
                 if 'auxgraytxt' in element.get('class', []):
@@ -56,18 +59,18 @@ class Scraper:
                 if 'verbtxt-term' in element.get('class', []):
                     word_constructor += element.get_text()
                     try:
-                        text[tense].append((pronoun, word_constructor, False))
+                        verb_dataset[tense].append((pronoun, word_constructor, False))
                     except KeyError:
                         return f"KeyError: TENSE WAS NOT FOUND, UNABLE TO INSERT WORD INTO DATABASE. \nDEBUG INFO: \nPronoun attempted to be inserted: {pronoun} \nWord attempted to be inserted: {word_constructor}"
                     word_constructor, pronoun = None, None
                 if 'verbtxt-term-irr' in element.get('class', []):
                     word_constructor += element.get_text()
                     try:
-                        text[tense].append((pronoun, word_constructor, True))
+                        verb_dataset[tense].append((pronoun, word_constructor, True))
                     except KeyError:
                         return f"KeyError: TENSE WAS NOT FOUND, UNABLE TO INSERT WORD INTO DATABASE. \nDEBUG INFO: \nPronoun attempted to be inserted: {pronoun} \nWord attempted to be inserted: {word_constructor}"
                     word_constructor, pronoun = None, None
-        return text
+        return self.global_dataset
     
     def parse(self):
         bs = self.get_html()
@@ -75,6 +78,15 @@ class Scraper:
         
 
 
-# x = scraper("https://conjugator.reverso.net/conjugation-portuguese-verb-poder.html")
+# x = Scraper("https://conjugator.reverso.net/conjugation-portuguese-verb-poder.html", verb="poder", global_dataset={})
+# # print(x.parse())
+
+# y = x.parse()
+# for v in y:
+#     print(v)
+#     for t in y[v]:
+#         print(t)
+#         for word in y[v][t]:
+#             print(word)
 
 
