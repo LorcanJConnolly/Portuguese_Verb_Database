4f211458327d6974f1b72f60d82aa88eddd7691c
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-07-30 16:05:34 +0100

    Initialised sample datasets for testing.

---
+++ sample_dataset.py
@@ -0,0 +1,9 @@
+# A sample database for testing.
+
+dataset_dict = {
+    "single_verb": {'verb': {'tense1': [('pronoun', 'conjugation', True)]}},
+    "multiple_verbs":{'verb1': {'tense1': [('pronoun', 'conjugation', True)]},
+                      'verb2': {'tense1': [('pronoun', 'conjugation', True)]},
+                      'verb3': {'tense1': [('pronoun', 'conjugation', True)]}},
+
+}
