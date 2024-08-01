374e7447f6b8b7fb15db9f2bee8f6cbbed4f17a5
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-08-01 14:53:03 +0100

    Added test_response_error_4XX and test_response_error_5XX for testing if client and server errors are caught during requests. Also added StartUp to TestHtmlExtraction for cleaner code.

--- unit_testing.py
+++ unit_testing.py
@@ -9,27 +9,60 @@ import MySQL_connection
 from sample_dataset import dataset_dict
 
 class TestHtmlExtraction(unittest.TestCase):
+    # FIXME: testing for different status codes using: https://httpbin.org
+
+    def setUp(self) -> None:
+        """
+        Initialise Scraper object for each test.
+        """
+        self.obj = Scraper(url="", verb="", global_dataset={})
 
     def test_missing_url(self):
-        """Test that an invalid or non-functioning url returns None."""
-        missing_url = Scraper(url="123", verb="", global_dataset={})
-        self.assertIsNone(missing_url.get_html())
+        """
+        Test that an invalid or non-functioning url returns None.
+        """
+        missing_url = "123"
+        self.assertIsNone(self.obj.get_html(missing_url))
 
     def test_sample_url(self):
-        """Test that a valid url from another webnsite returns a BeautifulSoup Object."""
-        sample_url = Scraper(url="https://docs.scrapy.org/en/latest/_static/selectors-sample1.html", verb="", global_dataset={})
-        self.assertIsInstance(sample_url.get_html(), BeautifulSoup)
+        """
+        Test that a valid url from another webnsite returns a BeautifulSoup Object.
+        """
+        sample_url = "https://docs.scrapy.org/en/latest/_static/selectors-sample1.html"
+        self.assertIsInstance(self.obj.get_html(sample_url), BeautifulSoup)
 
     def test_target_url(self):
-        """Test that a valid url from Reverso returns a BeautifulSoup Object."""
-        target_url = Scraper(url="https://conjugator.reverso.net/conjugation-portuguese-verb-ter.html", verb="", global_dataset={})
-        self.assertIsInstance(target_url.get_html(), BeautifulSoup)
+        """
+        Test that a valid url from Reverso returns a BeautifulSoup Object.
+        """
+        target_url = "https://conjugator.reverso.net/conjugation-portuguese-verb-ter.html"
+        self.assertIsInstance(self.obj.get_html(target_url), BeautifulSoup)
 
     def test_error_raised(self):    
-        """Test that checks correct Error is raised for missing url."""
-        missing_url = Scraper("123", verb="", global_dataset={})
+        """
+        Test that checks correct Error is raised for missing url.
+        """
+        self.obj.url = "123"
         with self.assertRaises(ValueError): 
-            missing_url.parse()
+            self.obj.parse()
+
+    def test_response_error_4XX(self):
+        """
+        Tests to see if 4XX client errors are caught when fetching a response from url.
+        """
+        error_codes = [400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451]
+        for error_code in error_codes:
+            url = f'https://httpbin.org/status/{error_code}'
+            self.assertIsNone(self.obj.get_html(url))
+
+    def test_response_error_5XX(self):
+        """
+        Tests to see if 5XX server errors are caught when fetching a response from url.
+        """
+        error_codes = [500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]
+        for error_code in error_codes:
+            url = f'https://httpbin.org/status/{error_code}'
+            self.assertIsNone(self.obj.get_html(url))
         
 class TestTextExtractionFromTags(unittest.TestCase):
     # FIXME: Test documentation.
@@ -129,12 +162,13 @@ class TestDatabaseStorage(unittest.TestCase):
     def test_database_created(self):
         pass
         # FIXME: if this test fails, final code not ran.
-        """ Tests if database is created and populated with tables. """
+        """ Tests if database is created """
         self.cursor.execute("SHOW DATABASES")
         databases = [database[0] for database in self.cursor.fetchall()]
         self.assertIn(self.test_db_name, databases)
 
     def test_database_populated(self):
+        """ Tests if database is created and populated with tables. """
         self.cursor.execute(f"USE {self.test_db_name}")
         self.cursor.execute("SHOW TABLES")
         tables = [table[0] for table in self.cursor.fetchall()]
@@ -144,6 +178,12 @@ class TestDatabaseStorage(unittest.TestCase):
         self.assertIn("pronoun", tables)
         self.assertIn("tense", tables)
         self.assertIn("conjugation", tables)
+        #TODO: Test tables are populated
+    
+    def test_database_idtables_populated(self):
+        """ Tests if database is created and ID tables are populated. """
+        pass
+
     
     def test_single_row_insertion(self):
         """ A test to see if data is correctly accepted and stored in the appropriate tables. """
