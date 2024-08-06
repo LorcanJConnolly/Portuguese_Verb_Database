bb6878be68ebd266a9316bc234c4a190d2e0ec03
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-08-06 12:58:53 +0100

    Created functionality: validate_db_name - Checks if given database name contains the correct characters for a MySQL database.  is_unique_name -  If creating a new database, checks if given database name already exists in the database.     If creating adding to an existing database, checks if given database name already exists in the database. exist_in_database - Checks if a verb exists in the database already.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # UI
+# TODO: clean up here. 
+#   Create an excel extractor
+#   Test inputs (multiple of the same verb).
 from scraper import Scraper
 from db_populator import Database
+from create_db import create_db
 import MySQL_connection
 
 """Create a delay between requests."""
 import time
 import random
+
+import argparse
+import os
+import re
 
 """
 self.connection = MySQL_connection.connect_to_mysql_server()
@@ -18,44 +26,89 @@ self.connection = MySQL_connection.connect_to_mysql_server()
             raise Exception(f"ERROR: Database '{self.test_db_name}' already exists. Aborting tests to prevent accidental data loss.")
 """
 
+def input_type(input):
+    if os.path.isfile(input) and os.path.splitext(input)[1] == ".xlsx" or os.path.splitext(input)[1] == ".xls":
+        # extract from excel.
+        # return verbs
+        pass
+    else:
+        return [verb.strip() for verb in input.split()]
+    
+def validate_db_name(name):
+    """ Checks if given database name contains the correct characters for a MySQL database. """
+    if re.match(r'^[A-Za-z0-9_]+$', name):
+        if is_unique_name(name):
+            return name
+        else:
+            raise ValueError(is_unique_name(name))
+    else:
+        raise argparse.ArgumentTypeError("Invalid database name. Must contain only alphanumeric characters and underscores, with no spaces.")
+
+def is_unique_name(name, create_new_databse):
+    """ Checks if given database name already exists in the database. """
+    connection = MySQL_connection.connect_to_mysql_server()
+    cursor = connection.cursor()
+    cursor.execute("SHOW DATABASES")
+    databases = [database[0] for database in cursor.fetchall()]
+    cursor.close()
+    connection.close()
+    if name in databases and create_new_databse == "Y":
+        return f"Database name: '{name}' already exists in the database. Please choose another name."
+    if name not in databases and create_new_databse == "N":
+        return f"Database name: '{name}' does not exist in the database. Please choose another name from: {databases}."
+    return True
+
+def exist_in_database(connection, verb):
+    cursor = connection.cursor()
+    cursor.execute(f"USE {args.database_name}")
+    cursor.execute("SELECT id FROM Verb WHERE Verb = %s", (verb,))
+    verb_id = cursor.fetchone()
+    cursor.close()
+    if verb_id is None:
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
-    # Give input verbs
-    verbs = ["acabar", "acalmar", "achar", "advinhar", "adorar", "ajudar", "almoçar", "amar", "analisar", "andar", "aniquilar", "apressar", "arrotar", "barbear", "bocejar", "buscar"]
+    """
+    
+    """
+    # python [file.py] [verbs] [database name] [create new database] // [JSON file name] [Create new JSON file]
+    parser = argparse.ArgumentParser(description="Scrape verb webpage(s) and insert data into a MySQL database.")
+    parser.add_argument("verbs", type=input_type, help="Input the verb(s) with a whitespace delimiter or the directory of an excel file containing only a list of verbs.")   # args.verbs
+    parser.add_argument("database_name", type=validate_db_name, help="The name of the database to populate. Must exist in databse if create_database is set to 'N'.")
+    parser.add_argument("create_new_databse", choices=['Y', 'N'], help="Y/N.\n'Y': create a new database with the name: [database_name].\n'N': add to an existing database with the name: [database_name].")
+    args = parser.parse_args()
+    
+    print(args)
+
+    connection = MySQL_connection.connect_to_mysql_server() 
+    if args.create_new_databse == "Y":
+        create_db(args.name, connection)
+    
     dataset = {}
-    for verb in verbs:
-        print("Scraping ", verb)
-        url = f"https://conjugator.reverso.net/conjugation-portuguese-verb-{verb}.html"
-        # def __init__(self, url, verb, global_dataset) -> None:
-        delay = random.uniform(15,25)
-        time.sleep(delay)
-        scraper = Scraper(url=url, verb=verb, global_dataset=dataset)
-        dataset = scraper.parse()
-    for v in dataset:
-        print(v)
 
-    # Please provid a database name or type NEWDATABASE to create a new database.
-    input_name = "NEWDATABASE"
-    input_create = False
-    if input_name == "NEWDATABASE":
-        # Give name of new db
-        input_name = "test_new"
-        input_create = True
-    
-    print("adding to db")
-    connection = MySQL_connection.connect_to_mysql_server()
-    # def __init__(self, connection, dataset, db_name=None, create_new_db=True) -> None:
-    populator = Database(connection=connection, db_name=input_name, create_new_db=input_create)
-    populator.populate_db(dataset=dataset)
+    for verb in args.verbs: 
+        if not exist_in_database(connection, verb):
+            print("Scraping the verb: ", verb)
+            url = f"https://conjugator.reverso.net/conjugation-portuguese-verb-{verb}.html"
+            time.sleep(random.uniform(10, 20))
+            scraper = Scraper(url=url, verb=verb, global_dataset=dataset)
+            # Insert the verb into database
+            populator = Database(connection=connection, db_name=args.database_name)
+            populator.populate_db(dataset, verb)
+            dataset = scraper.parse()
+        else:
+            print(f"Skpping the verb: '{verb}' as it already exists in the database.")
+
+    # Once done we extract to JSON file.
+
+
+
+
     
 
+ 
 
-# get db 
-# if new -> get name -> check if there's any conflict.
-# if not -> get db
-
-# get dataset from scraper
-# populate db using db_populator
-
-# close connection and cursor.
 
