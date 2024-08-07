94b3d2358352d06eaeeaf9069044c98810724895
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-08-07 11:20:12 +0100

    Converted from class object to script.

--- create_db.py
+++ create_db.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from dataset_structure import structure_dict
 
-def create_db(self, db_name, connection):
+def create_db(db_name, connection):
         """ 
         Initialise the database db_name and its 4 tables.
         Verb: An ID table for each verb stored in the database.
@@ -13,24 +13,24 @@ def create_db(self, db_name, connection):
 
         try:
             create_database_query = f"CREATE DATABASE IF NOT EXISTS {db_name};"
-            self.cursor.execute(create_database_query)
-            self.cursor.execute(f"USE {db_name}")
-            self.cursor.execute("""CREATE TABLE Verb(
+            cursor.execute(create_database_query)
+            cursor.execute(f"USE {db_name}")
+            cursor.execute("""CREATE TABLE Verb(
                         id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
                         Verb VARCHAR(21) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci UNIQUE)
                         """)   # Longest Portuguese root veb: anticonstitucionalizar
-            self.connection.commit()
-            self.cursor.execute("""CREATE TABLE Tense(
+            connection.commit()
+            cursor.execute("""CREATE TABLE Tense(
                         id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
                         Tense VARCHAR(63) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci UNIQUE)
                         """)
-            self.connection.commit()
-            self.cursor.execute("""CREATE TABLE Pronoun(
+            connection.commit()
+            cursor.execute("""CREATE TABLE Pronoun(
                         id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
                         Pronoun VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci UNIQUE)
                         """)
-            self.connection.commit()
-            self.cursor.execute("""CREATE TABLE Conjugation(
+            connection.commit()
+            cursor.execute("""CREATE TABLE Conjugation(
                         id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
                         Verb INT UNSIGNED, 
                         Tense TINYINT UNSIGNED, 
@@ -41,20 +41,20 @@ def create_db(self, db_name, connection):
                         FOREIGN KEY (Tense) REFERENCES Tense(id),
                         FOREIGN KEY (Pronoun) REFERENCES Pronoun(id))
                         """) 
-            self.connection.commit()
-            self.cursor.execute("INSERT IGNORE INTO Pronoun (Pronoun) VALUES (NULL)")   # Insert a Null ID reference for conguations without pronouns.
+            connection.commit()
+            cursor.execute("INSERT IGNORE INTO Pronoun (Pronoun) VALUES (NULL)")   # Insert a Null ID reference for conguations without pronouns.
 
             """ Populated Key Tables. """
             for tense, pronouns in structure_dict.items():
-                self.cursor.execute("INSERT INTO Tense (Tense) VALUES (%s)", (tense,))
-                self.connection.commit()
+                cursor.execute("INSERT INTO Tense (Tense) VALUES (%s)", (tense,))
+                connection.commit()
                 if pronouns is not None:
                     for pronoun in pronouns:
-                        self.cursor.execute("INSERT IGNORE INTO Pronoun (Pronoun) VALUES (%s)", (pronoun,))   # Ignore duplicates.
-                        self.connection.commit()
-
+                        cursor.execute("INSERT IGNORE INTO Pronoun (Pronoun) VALUES (%s)", (pronoun,))   # Ignore duplicates.
+                        connection.commit()
 
         except mysql.connector.Error as error:
             print(f"ERROR CREATING DATABASE\nDEBUG INFO\nError number: {error.errno}\nError message: {error.msg}")
-            self.cursor.execute(f"DROP DATABASE {db_name}")
+            # if it exists in the database
+            cursor.execute(f"DROP DATABASE IF EXISTS {db_name}")
     
