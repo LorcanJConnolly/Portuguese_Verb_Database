0b8550fbf8a6969647eb8b34e98a5ac7d42c59d1
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2024-08-09 12:26:29 +0100

    Implemented using openpyxl.

--- excel_to_list.py
+++ excel_to_list.py
@@ -1,2 +1,18 @@
 """ Extracts a column of verbs as a list. """
-pass
+import openpyxl
+
+def extract(path):
+    list = []
+    wb_obj = openpyxl.load_workbook(path)
+    ws_obj = wb_obj.active
+    for row in ws_obj.iter_rows(max_col=1):
+        try:
+            for cell in row:
+                if not isinstance(cell.value, str):
+                    raise ValueError(f"\nERROR: Verbs must be strings.\n---DEBUG INFO:\n------Wrong input: '{cell.value}'\n")
+                else: 
+                    list.append(cell.value)
+        except ValueError as e:
+            print(e)
+            continue 
+    return list
